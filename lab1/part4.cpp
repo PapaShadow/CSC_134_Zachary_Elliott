b94bc5717cba866178db10e32f21af7e1e5f15bd
PapaShadow <elliottzj689@mail.sandhills.edu>
2025-08-25 12:14:59 -0400

    finished lab 1 part 4

---
+++ lab1/part4.cpp
@@ -0,0 +1,17 @@
+// Lab 1 Part 4
+// Course: CSC 134
+// Name: Zachary Elliott
+// Date: 08-25-25
+
+#include <iostream> //calls the iostream library for input and output operations
+using namespace std; //calls the standard namespace
+
+int main() // main function where the program starts execution
+{
+    cout << " My favortie movie is \'How to Train Your Dragon\'." << endl; // spit out result
+    cout << " Starring: \n Jay Baruchel as Hiccup." << endl; // spit out result
+    cout << " Gerard Butler as Stoick." << endl; // spit out result
+    cout << " America Ferrera as Astrid." << endl; // spit out result
+
+    return 0; // returns 0 to indicate successful execution
+}
