f763cb87d2f51d734846592e159d16b22f524ba3
PapaShadow <elliottzj689@mail.sandhills.edu>
2025-09-03 10:24:24 -0400

    fnished week 3 lab

--- lab3/new_averages.cpp
+++ lab3/new_averages.cpp
@@ -10,5 +10,24 @@ double find_new_average(double, double);
 
 int main() //main function
 {
+    double new_york = 85,
+            denver = 88,
+            phoenix = 106,
+            avg_increase = 1.02;
+
+    std::cout << "The new average temp for New York after a 2% increase is "
+        << find_new_average(new_york, avg_increase) << " degrees." << std::endl;
+    
+    std::cout << "The new average temp for Denver after a 2% increase is "
+        << find_new_average(denver, avg_increase) << " degrees." << std::endl;
+    
+    std::cout << "The new average temp for Phoenix after a 2% increase is "
+        << find_new_average(phoenix, avg_increase) << " degrees." << std::endl;
+
     return 0;
+}
+
+double find_new_average(double temp, double increase)
+{
+    return temp * increase;
 }
