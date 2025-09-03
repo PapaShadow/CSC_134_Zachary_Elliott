11628689985d4f1ca3f9586cd52abef8bc600070
PapaShadow <elliottzj689@mail.sandhills.edu>
2025-09-03 08:04:29 -0400

    started lab 3

---
+++ lab3/new_averages.cpp
@@ -0,0 +1,14 @@
+//@author: Zachary Elliott
+//@date: 09-25
+//@purpose: Lab 3, finds a new average if temps go up 2%
+
+#include <iostream> //calls the iostream library for input and output operations
+using namespace std; //calls the standard namespace
+
+// Function prototypes
+double find_new_average(double, double);
+
+int main() //main function
+{
+    return 0;
+}
