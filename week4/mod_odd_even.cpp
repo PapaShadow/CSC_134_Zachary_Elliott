644ca3da7f924fbf47c5c3c7e8960903fbe16d07
PapaShadow <elliottzj689@mail.sandhills.edu>
2025-09-10 11:50:25 -0400

    started week 4 lab 3

---
+++ week4/mod_odd_even.cpp
@@ -0,0 +1,13 @@
+//@author: Zachary Elliott
+//@date: 09-10-25
+//@purpose: Week 4 lab 2, prompmts user for grades then determines letter grade
+
+#include <iostream> //calls the iostream library for input and output operations
+
+using namespace std; //calls the standard namespace
+
+int main()
+{
+
+    return 0;
+}
