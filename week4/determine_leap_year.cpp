0965793f8f837e116fff5b5f9379cc22c683fc52
PapaShadow <elliottzj689@mail.sandhills.edu>
2025-09-12 17:35:37 -0400

    finished lab 5 for week 4

---
+++ week4/determine_leap_year.cpp
@@ -0,0 +1,45 @@
+//@author: Zachary Elliott
+//@date: 09-12-25
+//@purpose: Week 4 lab 5, prompmts user for a year then determines if it's a leap year
+// keeping this one super simple, because i am very tired and have a headache
+// also the dude im working with smells like runners feet and wet dog armpits
+
+
+#include <iostream>
+using namespace std;    
+
+int main()
+{
+    // variable to hold user input
+    // set to 0000 so if user enters nothing it will be invalid
+    int userYear = 0000;
+
+    // print statement with user prompt
+    cout << "Enter a 4 digit year, for example 2024: ";
+    cin >> userYear;
+
+    // if series to check if what the user entered is a leap year
+    // if divisible by 4 then it is a leap year
+    if (userYear % 4 == 0)
+    {
+        // if divisible by 100 then it is NOT a leap year
+        if (userYear % 100 == 0)
+        {
+            // But if it is a multiple of 400 then it IS a leap year
+            if (userYear % 400 == 0)
+            {
+                cout << userYear << " is a leap year." << endl;
+            }
+            else
+            {
+                cout << userYear << " is not a leap year." << endl;
+            }
+        }
+    }
+    else
+    {
+        cout << userYear << " is not a leap year." << endl;
+    }
+
+    return 0;
+}
