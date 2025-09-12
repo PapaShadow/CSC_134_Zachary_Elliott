5ec1f74b7c34ec836e7932bcf3f2978e2c6a0409
PapaShadow <elliottzj689@mail.sandhills.edu>
2025-09-12 16:04:14 -0400

    finished week 4 lab 3 and started lab 4

--- week4/mod_odd_even.cpp
+++ week4/mod_odd_even.cpp
@@ -1,13 +1,61 @@
 //@author: Zachary Elliott
-//@date: 09-10-25
-//@purpose: Week 4 lab 2, prompmts user for grades then determines letter grade
+//@date: 09-12-25
+//@purpose: Week 4 lab 3, prompmts user for a number then uses modulo to determine if odd or even
 
 #include <iostream> //calls the iostream library for input and output operations
 
 using namespace std; //calls the standard namespace
 
+//function prototypes
+int checkNumber(int);
+int promptUser();
+
 int main()
 {
+    //variable declaration
+    int userInput = 0;
+
+    // call function and assign to variable
+    userInput = promptUser();
+
+    //call function for check
+    checkNumber(userInput);
 
     return 0;
+}
+
+int checkNumber(int userInput)
+{
+    // making a constant for safety
+    // 0 is always even
+    const int EVEN_NUMBER = 0;
+
+    //keeping this just incase i need it.
+    const int ODD_NUMBER = 1;
+
+    //just a modulos check
+    if (userInput % 2 == EVEN_NUMBER)
+    {
+        // if even shoot out this message
+        cout << userInput << " is an even number." << endl;
+    }
+    else
+    {
+        // if odd shoot out this message
+        // shouldnt need to check since there are only two options
+        cout << userInput <<" is an odd number." << endl;
+    }
+    return 0;
+}
+
+int promptUser()
+{
+    // variable to store user input
+    int userInput = 0;
+
+    // prompt for user input
+    cout << "Please enter a number: " << endl;
+    cin >> userInput; // store user input in variable
+
+    return userInput;
 }
---
+++ week4/vowel_or_consonant.cpp
@@ -0,0 +1,13 @@
+//@author: Zachary Elliott
+//@date: 09-12-25
+//@purpose: Week 4 lab 4, prompmts user for a character and determines if vowel or consonant
+
+#include <iostream> //calls the iostream library for input and output operations
+
+using namespace std; //calls the standard namespace
+
+int main()
+{
+    
+    return 0;
+}
