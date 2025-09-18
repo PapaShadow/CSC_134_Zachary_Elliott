4d2e253bfadc50adc35b07090cb689444e41fc34
PapaShadow <elliottzj689@mail.sandhills.edu>
2025-09-18 16:48:29 -0400

    finished grade avg lab

--- week5/grade_average.cpp
+++ week5/grade_average.cpp
@@ -12,13 +12,17 @@ double calcAverage(double grades[], int size);
 
 int main()
 {
+    // make variables and initialize
     double average = 0.0;
-    const int SIZE = 10;
-    double gradeArray[SIZE];
+    const int SIZE = 10; // hoard code size, Can always use this later to make array more dynamic
+    double gradeArray[SIZE]; // define array
 
-   for (int i = 1; i <= 10; i++)
+    // for loop to get user grades
+   for (int i = 0; i < 10; i++)
    {
-        std::cout << "Please enter grade " << i << ":";
+        // add +1 to i so it reads as grade 1, grade 2, etc
+        // sticks user input into the array index that matches i
+        std::cout << "Please enter grade " << (i+1) << ":";
         std::cin >> gradeArray[i];
    }
 
@@ -33,10 +37,14 @@ double calcAverage(double grades[], int size)
 {
     double sum = 0.0;
 
+    // loop through array and add all values together
+    // stores values into sum while adding it each time
     for (int i = 0; i < size; i++)
     {
         sum += grades[i];
     }
+    // returns sum divided by size as a double
+    // have to cast size as a double to make sure it is a double on ouput
     return sum / static_cast<double>(size);
 }
 
