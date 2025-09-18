d2b3e2844d700a429521259e8b42ea11ed9b0f6a
PapaShadow <elliottzj689@mail.sandhills.edu>
2025-09-18 17:12:56 -0400

    added notes and finisehd the temp loop lab

--- week5/loop_counter.cpp
+++ week5/loop_counter.cpp
@@ -2,3 +2,4 @@
 //@date: 09-15-25
 //@purpose: Week 5 lab 5, create a loop to count 100 times
 #include <iostream>
+using namespace std;
--- week5/temp_loop.cpp
+++ week5/temp_loop.cpp
@@ -1,4 +1,51 @@
 //@author: Zachary Elliott
-//@date: 09-15-25
+//@date: 09-16-25
 //@purpose: Week 5 lab 4, a program that loops until the user enters the correct number
 #include <iostream>
+
+using namespace std;
+
+// function prototype
+bool checkInput(double);
+
+int main()
+{
+    // initialive user variable as double
+    // also forces the loop to start siince it is not the right value
+    double userDegree = 0.0;
+    
+
+    // loop until the user gets it right
+    // uses not equals because i prefer it
+    // calls function to check inout
+    while (checkInput(userDegree) != true)
+    {
+        std::cout << "Please guess a tempature: ";
+        std::cin >> userDegree;
+    }
+
+    // output success message
+    std:: cout << "You have guessed correctyl!" << std::endl;
+
+    return 0;
+}
+
+// simple bool function to do our check
+bool checkInput(double userDegree)
+{
+    // correct temp is a constant
+    double const CORRECT_TEMP = 212.34;
+    // bool variable to pass around
+    bool guessCorrect = false;
+
+    if (userDegree != CORRECT_TEMP)
+    {
+        guessCorrect = false;
+    }
+    else
+    {
+        guessCorrect = true;
+    }
+
+    return guessCorrect;
+}
