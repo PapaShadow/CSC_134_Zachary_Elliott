2dc1c150f3d5a112abaa6c9b88f3d71be0f51383
PapaShadow <elliottzj689@mail.sandhills.edu>
2025-10-01 12:15:44 -0400

    finished lab 4 week8

--- week8/lab4.cpp
+++ week8/lab4.cpp
@@ -7,3 +7,48 @@
   each value on a new line without any spaces between each line. Make sure you use the close() method to
    close the file and then print "Done Writing...." to the console when the program is complete.
 */
+//@author: Zachary Elliott
+//@date: 10-01-25
+//@purpose: Week 8 lab 1, using ofstream object to write some info into a text file
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int getUserInput();
+
+int main()
+{
+    //variable declarations
+    //creating string variable for an easy to use path in all future code
+    //this should make life easier for you
+    string filePath = "C:\\Users\\shado\\CSC_134_Zachary_Elliott\\week8";
+    string fileName = "\\lab4.txt";
+    
+    int userVariable = getUserInput();
+    //open file
+    ofstream input_file(filePath + fileName);
+
+    for(int i = 0; i < userVariable; i++)
+    {
+      //calls file object to write current loop index  
+      input_file << i << "\n";
+    }
+    //close file to free up memory
+    input_file.close();
+    
+    std::cout << "File is closed and done being written to." << std::endl;
+
+    return 0;                        
+}
+
+// simple function to get user input to count rows
+int getUserInput()
+{
+    int userCount = 0;
+
+    std::cout << "Enter the number of loops you would like: ";
+    std::cin >> userCount;
+
+    return userCount;
+}
